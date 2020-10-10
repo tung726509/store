6ec1a3f21eba7f552df550909f5f65a1cc9f8dab
tungcntt186 <tungcntt186@gmail.com>
2020-10-10 08:17:14 +0700

    realease

--- public/homepage/js/head-js-1.js
+++
@@ -1,9 +0,0 @@
-WebFontConfig = {
-  google: { families: [ 'Open+Sans:200,300,400,500,600,700,800','Poppins:200,300,400,500,600,700,800','Oswald:400,600,700' ] }
-  };
-  (function(d) {
-  var wf = d.createElement('script'), s = d.scripts[0];
-  wf.src = 'http://store.min:81/homepage/js/webfont.js';
-  wf.async = true;
-  s.parentNode.insertBefore(wf, s);
-  })(document);
--- public/homepage/js/homepage-app-2.js
+++
@@ -1,8 +0,0 @@
-if ('off' === jQuery.cookie('porto_ads_status')) {
-} else {
-	jQuery('.porto-block-html-top > div').removeClass('d-none').append('<button class="mfp-close"></button>');
-}
-jQuery('body').on('click', '.porto-block-html-top .mfp-close', function() {
-		jQuery(this).parent().fadeOut();
-		jQuery.cookie('porto_ads_status', 'off', { expires : 7 });
-});
