752d07f65a9058e717d7e1a8bdcc386400cd2bd4
VU SON TUNG <tungvs@kaopiz.com>
2022-06-08 09:06:23 +0700

    test job send mail 2

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Arr;
 use App\Rules\VietnamesePhone;
 use App\Rules\PasswordCorrect;
 use App\Jobs\SendEmail;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\MailNotify;
 
 use App\User;
@@ -27,19 +27,20 @@ class UserController extends Controller
         $this->role_m = new Role();
     }
 
-    public function index(){
-    	$per_page = request()->query('per', 10);
-        $search = request()->query('s','');
+    public function index()
+    {
+        $per_page = request()->query('per', 10);
+        $search = request()->query('s', '');
         $param = [];
 
         $query = $this->_m->with([]);
 
-        if($search != null && $search != ""){
+        if ($search != null && $search != "") {
             $param['code'] = $search;
         }
 
         $query = $query->filter($param);
-        $users = $query->orderBy('created_at','asc')->paginate($per_page);
+        $users = $query->orderBy('created_at', 'asc')->paginate($per_page);
 
         // test
         $users1 = User::all();
@@ -49,20 +50,21 @@ class UserController extends Controller
             'content' => 'has been created!',
         ];
         $user = Auth::user();
+        // dd(Carbon::now());
         // cách 1 : gọi qua 1 cái job và đây là các đi kèm release() , delay() , attempts() viết sau dispatch
-        // SendEmail::dispatch($message, $users1);
+        SendEmail::dispatch($message, $users1)->delay(Carbon::now()->addMinutes(2));
         // cách 2 gọi trực tiếp 1 cái mail
         // Mail::to($user->email)->send(new MailNotify($message));
         // end test
 
-    	return view('admin.user.index',compact('per_page','search','users'));
+        return view('admin.user.index', compact('per_page', 'search', 'users'));
     }
 
     public function getAdd()
     {
-    	$roles = $this->role_m->get();
+        $roles = $this->role_m->get();
 
-    	return view('admin.user.add',compact('roles'));
+        return view('admin.user.add', compact('roles'));
     }
 
     public function add(Request $request)
@@ -70,15 +72,15 @@ class UserController extends Controller
         $req = $request->all();
 
         $rules = [
-            'username' => ['required','string','unique:users,username'],
-            'name' => ['nullable','string','max:255'],
-            'email' => ['nullable','email','unique:users,email'],
-            'role' => ['required','exists:roles,name'],
+            'username' => ['required', 'string', 'unique:users,username'],
+            'name' => ['nullable', 'string', 'max:255'],
+            'email' => ['nullable', 'email', 'unique:users,email'],
+            'role' => ['required', 'exists:roles,name'],
             'current_password' => [new PasswordCorrect],
-            'password' => ['required','string','min:6','max:255','confirmed'],
-            'fb' => ['nullable','url','max:255'],
-            'ins' => ['nullable','url','max:255'],
-            'skype' => ['nullable','url','max:255'],
+            'password' => ['required', 'string', 'min:6', 'max:255', 'confirmed'],
+            'fb' => ['nullable', 'url', 'max:255'],
+            'ins' => ['nullable', 'url', 'max:255'],
+            'skype' => ['nullable', 'url', 'max:255'],
         ];
 
         $messages = [
@@ -106,9 +108,9 @@ class UserController extends Controller
 
         $validator = Validator::make($request->all(), $rules, $messages);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return back()->withInput()->withErrors($validator);
-        }else{
+        } else {
             $data['username'] = $request->input('username');
             $data['name'] = $request->input('name');
             $data['email'] = $request->input('email');
@@ -121,14 +123,14 @@ class UserController extends Controller
 
             $created = $this->_m->create($data);
 
-            if($created){
+            if ($created) {
                 // dd($created);
                 $created->syncRoles($request->input('role'));
 
                 $url = url()->current();
-                return redirect($url)->with(['success'=>'Thêm mới thành công !']);
-            }else{
-                return back()->withInput()->with('fail','Lỗi hệ thống , vui lòng thử lại sau !');
+                return redirect($url)->with(['success' => 'Thêm mới thành công !']);
+            } else {
+                return back()->withInput()->with('fail', 'Lỗi hệ thống , vui lòng thử lại sau !');
             }
         }
     }
@@ -136,155 +138,154 @@ class UserController extends Controller
     public function getEdit($id)
     {
         $user = $this->_m->with([])->find($id);
-            
-        if($user){
+
+        if ($user) {
             $role = $user->getRoleNames()->first();
             $role_pretty_name = $user->getRolePrettyNames()->first();
-            $social_network = json_decode($user->social_network,true);
+            $social_network = json_decode($user->social_network, true);
             $roles = $this->role_m->get();
             $form_id = null;
 
-            return view('admin.user.edit',compact('user','role','role_pretty_name','roles','social_network','form_id'));
-        }else{
+            return view('admin.user.edit', compact('user', 'role', 'role_pretty_name', 'roles', 'social_network', 'form_id'));
+        } else {
             $messages = 'Không tìm thấy gì cả =((';
-            return view('admin.alertpages.404',compact('messages'));
+            return view('admin.alertpages.404', compact('messages'));
         }
     }
 
-    public function edit(Request $request,$id)
-    {	
-    	$req = $request->all();
-    	$rules = [];
+    public function edit(Request $request, $id)
+    {
+        $req = $request->all();
+        $rules = [];
         $messages = [];
-    	// dd($req);
+        // dd($req);
 
-    	if(Arr::has($req,'accountForm')){
-    		$rules = [
-	            'username' => ['required','string','unique:users,username,'.$id.',id'],
-	            'name' => ['nullable','string','max:255'],
-	            'email' => ['nullable','email','unique:users,email,'.$id.',id'],
-	            'role' => ['required','exists:roles,name'],
-	        ];
+        if (Arr::has($req, 'accountForm')) {
+            $rules = [
+                'username' => ['required', 'string', 'unique:users,username,' . $id . ',id'],
+                'name' => ['nullable', 'string', 'max:255'],
+                'email' => ['nullable', 'email', 'unique:users,email,' . $id . ',id'],
+                'role' => ['required', 'exists:roles,name'],
+            ];
 
-	        $messages = [
-	            'username.required' => 'Vui lòng nhập tên đăng nhập !',
-	            'username.string' => 'Chỉ nhập chuỗi !',
-	            'username.unique' => 'Tên đăng nhập đã được sử dụng !',
-	            'name.string' => 'Chỉ nhập chuỗi !',
-	            'name.max' => 'Tối đa 255 kí tự',
-	            'email.email' => 'Email không hợp lệ !',
+            $messages = [
+                'username.required' => 'Vui lòng nhập tên đăng nhập !',
+                'username.string' => 'Chỉ nhập chuỗi !',
+                'username.unique' => 'Tên đăng nhập đã được sử dụng !',
+                'name.string' => 'Chỉ nhập chuỗi !',
+                'name.max' => 'Tối đa 255 kí tự',
+                'email.email' => 'Email không hợp lệ !',
                 'email.unique' => 'Email đã được sử dụng !',
                 'role.required' => 'Vui lòng chọn quyền hạn cho tài khoản !',
-	            'role.exists' => 'Quyền hạn không tồn tại !',
-	        ];
-    	}elseif (Arr::has($req,'passwordChangeForm')) {
-    		$rules = [
-	            'current_password' => [new PasswordCorrect],
-	            'password' => ['required','string','min:6','max:255','confirmed'],
-	        ];
+                'role.exists' => 'Quyền hạn không tồn tại !',
+            ];
+        } elseif (Arr::has($req, 'passwordChangeForm')) {
+            $rules = [
+                'current_password' => [new PasswordCorrect],
+                'password' => ['required', 'string', 'min:6', 'max:255', 'confirmed'],
+            ];
 
-	        $messages = [
-	            'password.required' => 'Vui lòng nhập mật khẩu !',
-	            'password.string' => 'Chỉ nhập chuỗi !',
-	            'password.min' => 'Tối thiểu 6 kí tự !',
-	            'password.max' => 'Tối đa 255 kí tự !',
-	            'password.confirmed' => 'Mật khẩu nhập lại không trùng khớp !',
-	        ];
-    	}elseif (Arr::has($req,'contactForm')) {
-    		$rules = [
-	            'fb' => ['nullable','url','max:255'],
-	            'ins' => ['nullable','url','max:255'],
-	            'skype' => ['nullable','url','max:255'],
-	        ];
+            $messages = [
+                'password.required' => 'Vui lòng nhập mật khẩu !',
+                'password.string' => 'Chỉ nhập chuỗi !',
+                'password.min' => 'Tối thiểu 6 kí tự !',
+                'password.max' => 'Tối đa 255 kí tự !',
+                'password.confirmed' => 'Mật khẩu nhập lại không trùng khớp !',
+            ];
+        } elseif (Arr::has($req, 'contactForm')) {
+            $rules = [
+                'fb' => ['nullable', 'url', 'max:255'],
+                'ins' => ['nullable', 'url', 'max:255'],
+                'skype' => ['nullable', 'url', 'max:255'],
+            ];
 
-	        $messages = [
-	        	'fb.url' => 'Link không hợp lệ !',
-	        	'fb.max' => 'Tối đa 255 kí tự !',
-	        	'ins.url' => 'Link không hợp lệ !',
-	        	'fb.max' => 'Tối đa 255 kí tự !',
-	        	'skype.url' => 'Link không hợp lệ !',
-	        	'skype.max' => 'Tối đa 255 kí tự !',
-	        ];
-    	}elseif (Arr::has($req,'otherForm')) {
-    	}
+            $messages = [
+                'fb.url' => 'Link không hợp lệ !',
+                'fb.max' => 'Tối đa 255 kí tự !',
+                'ins.url' => 'Link không hợp lệ !',
+                'fb.max' => 'Tối đa 255 kí tự !',
+                'skype.url' => 'Link không hợp lệ !',
+                'skype.max' => 'Tối đa 255 kí tự !',
+            ];
+        } elseif (Arr::has($req, 'otherForm')) {
+        }
 
         $validator = Validator::make($request->all(), $rules, $messages);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return back()->withInput()->withErrors($validator);
-        }else{
-        	$data = [];
-        	$form_id = '';
+        } else {
+            $data = [];
+            $form_id = '';
             $user = $this->_m->find($id);
-            if(is_null($user)){
-                return back()->withInput()->with('fail','User này không tồn tại !');
+            if (is_null($user)) {
+                return back()->withInput()->with('fail', 'User này không tồn tại !');
             }
 
-        	if($request->has('accountForm')){
-        		$data['username'] = $request->input('username');
-        		$data['name'] = $request->input('name');
-        		$data['email'] = $request->input('email');
-        		$form_id = 'account';
-        	}elseif($request->has('passwordChangeForm')){
-        		$data['password'] = bcrypt($request->input('password'));
-        		$form_id = 'password_change';
-        	}elseif($request->has('contactForm')){
-        		$social_network['fb'] = $request->input('fb');
-        		$social_network['ins'] = $request->input('ins');
-        		$social_network['skype'] = $request->input('skype');
-        		$data['social_network'] = json_encode($social_network);
-        		$form_id = 'contact';
-        	}elseif($request->has('otherForm')){
-        		
-        	}
+            if ($request->has('accountForm')) {
+                $data['username'] = $request->input('username');
+                $data['name'] = $request->input('name');
+                $data['email'] = $request->input('email');
+                $form_id = 'account';
+            } elseif ($request->has('passwordChangeForm')) {
+                $data['password'] = bcrypt($request->input('password'));
+                $form_id = 'password_change';
+            } elseif ($request->has('contactForm')) {
+                $social_network['fb'] = $request->input('fb');
+                $social_network['ins'] = $request->input('ins');
+                $social_network['skype'] = $request->input('skype');
+                $data['social_network'] = json_encode($social_network);
+                $form_id = 'contact';
+            } elseif ($request->has('otherForm')) {
+            }
 
-        	$updated = $this->_m->where('id',$id)->update($data);
+            $updated = $this->_m->where('id', $id)->update($data);
 
-        	if($updated){
-                if($request->has('accountForm')){
+            if ($updated) {
+                if ($request->has('accountForm')) {
                     $user->syncRoles($request->input('role'));
                 }
 
-        		$url = url()->current();
+                $url = url()->current();
                 // dd($form_id);
-                return redirect($url)->with(['success'=>'Cập nhật thành công !','form_id'=>$form_id]);
-            }else{
-                return back()->withInput()->with('fail','Lỗi hệ thống , vui lòng thử lại sau !');
+                return redirect($url)->with(['success' => 'Cập nhật thành công !', 'form_id' => $form_id]);
+            } else {
+                return back()->withInput()->with('fail', 'Lỗi hệ thống , vui lòng thử lại sau !');
             }
-    	}
+        }
     }
 
     public function lockUnlock(Request $request)
     {
-        if($request->ajax()){
+        if ($request->ajax()) {
             $current_acc = Auth::user()->roles->first()->name;
-            if($current_acc != 'admin'){
-                return Response::json(['success'=>true,'message'=>'Tài khoản của bạn không có quyền thực hiện hành động này .']);
-            }else{
+            if ($current_acc != 'admin') {
+                return Response::json(['success' => true, 'message' => 'Tài khoản của bạn không có quyền thực hiện hành động này .']);
+            } else {
 
                 $action = $request->action;
                 $user_id = $request->user_id;
 
-                if($action == 'lock' || $action == 'unlock'){
+                if ($action == 'lock' || $action == 'unlock') {
                     $user = $this->_m->find($user_id);
-                    if($user){
+                    if ($user) {
                         $message = '';
                         $lock = null;
-                        if($action == 'lock'){
+                        if ($action == 'lock') {
                             $lock = 0;
                             $message = 'locked';
-                        }else{
+                        } else {
                             $lock = 1;
                             $message = 'unlocked';
                         }
 
-                        $updated = $this->_m->where('id',$user_id)->update(['lock'=>$lock]);
+                        $updated = $this->_m->where('id', $user_id)->update(['lock' => $lock]);
 
-                        if($updated){
-                            return Response::json(['success'=>true,'message'=>$message]);
+                        if ($updated) {
+                            return Response::json(['success' => true, 'message' => $message]);
                         }
-                    }else{
-                        return Response::json(['success'=>true,'message'=>'Tài khoản không tồn tại .']);
+                    } else {
+                        return Response::json(['success' => true, 'message' => 'Tài khoản không tồn tại .']);
                     }
                 }
             }
